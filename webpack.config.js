872f442f5049e93e9ad5dc3048a74d26433104c9
entropy82 <froutopotos.alex@gmail.com>
2017-11-27 17:38:37 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+const webpack =  require('webpack');
+const path = require('path');
+const TransferWebpackPlugin = require('transfer-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-plugin');
+
